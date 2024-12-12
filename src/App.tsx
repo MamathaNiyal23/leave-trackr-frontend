29a210106372628366f28f31397f8153119a2bca
Mamatha <mamatha@Admin-MacBook-Pro.local>
2024-12-12 22:01:56 +0530

    Add App to render hompage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import Home from './page/HomePage/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  
+import SignUp from './page/RegistrationPage/Register';
+import Login from './page/LoginPage/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          LeaveTrackr
-        </p>
-      </header>
-    </div>
+    <Router >
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
   );
+
+
 }
 
 export default App;

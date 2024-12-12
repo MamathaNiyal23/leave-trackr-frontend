2450b3c2c6348ab27f646a6a910b848dbd62445f
Mamatha <mamatha@Admin-MacBook-Pro.local>
2024-12-12 22:45:58 +0530

    Update App with adding DashBoard route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './page/HomePage/Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";  
 import SignUp from './page/RegistrationPage/Register';
 import Login from './page/LoginPage/Login';
+import DashBoard from './page/DashBoard/DashBoard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<DashBoard/>}/>
         </Routes>
       </div>
     </Router>

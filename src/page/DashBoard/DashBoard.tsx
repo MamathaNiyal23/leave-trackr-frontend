c2a3f6fded4dd4816745e88543d773a755af4afa
Mamatha <mamatha@Admin-MacBook-Pro.local>
2024-12-18 13:11:43 +0530

    Add DashBoard file

--- src/page/DashBoard/DashBoard.tsx
+++ src/page/DashBoard/DashBoard.tsx
@@ -1,11 +1,28 @@
-import React from "react";
 
-const DashBoard = () =>{
-    return(
-        <div>
-            <h5>Dashboard</h5>
-        </div>
-    )
+import React, { useEffect, useState } from 'react';
+import './DashBoard.css';
+import UserDashboard from './UserDashBoard';
+import AdminDashboard from './AdminDashBoard';
+
+const Dashboard: React.FC = () => {
+  const storedUser = localStorage.getItem('user');
+
+  const user = storedUser ? JSON.parse(storedUser) : null;
+
+  if (!user) {
+    return <p>Please log in</p>;
+  }
+
+  return (
+    <div className="dashboard">
+      
+      {user.role === 'user' ? (
+        <UserDashboard userId={user.userId} username={user.username} />
+      ) : (
+        <AdminDashboard userId={user.userId}  username={user.username}/>
+      )}
+    </div>
+  );
 };
 
-export default DashBoard;
+export default Dashboard;

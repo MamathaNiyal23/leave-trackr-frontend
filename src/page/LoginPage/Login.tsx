af995c4cc3d0993f4b454132b8ffb35b6b7d42a1
Mamatha <mamatha@Admin-MacBook-Pro.local>
2024-12-18 13:02:19 +0530

    Update Login page

--- src/page/LoginPage/Login.tsx
+++ src/page/LoginPage/Login.tsx
@@ -6,15 +6,38 @@ const Login = () => {
   const { register, handleSubmit } = useForm();
   const navigate = useNavigate();
 
-  const onSubmit = () => {
-    navigate("/dashboard");
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_BASE_URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        const { id, username, role } = responseData;
+
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ userId: id, username, role })
+        );
+        navigate("/dashboard");
+      } else {
+        const errorData = await response.json();
+        alert(errorData.error || "Login failed. Please try again.");
+      }
+    } catch (error) {
+      alert("An error occurred. Please try again later.");
+    }
   };
   return (
     <div className="container">
       <div>
         <img src="/bg1.png" alt="Background" />
       </div>
-
       <div className="right">
         <h1>Hello again! 👋 </h1>
         <p>Login to your account</p>
@@ -29,10 +52,8 @@ const Login = () => {
             placeholder="Password"
             {...register("password", { required: "password is required" })}
           />
-
           <button type="submit">Login</button>
         </form>
-
         <div>
           <h4>
             Don't have an account? <Link to="/signup">Sign Up</Link>
